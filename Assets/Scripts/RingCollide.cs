dc3123b13e4db5062ffe2827c811724c4bc26e8a
koalasushi <kai.daniel.travis@gmail.com>
2025-04-21 00:55:46 +0900

    めちゃくちゃ簡単ななにかを作った
    力尽きた

---
+++ Assets/Scripts/RingCollide.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RingCollide : MonoBehaviour
+{
+    public float activatedSeconds = 1;
+    private Color basecolour;
+    private Renderer tower;
+    private bool activated = false;
+    
+    void Start(){
+        tower = GetComponent<Renderer>();
+        basecolour = tower.material.color;
+    }
+
+    void OnTriggerEnter(Collider ring){
+        Debug.Log("Collision detected with tag: " + ring.tag);
+        if(ring.CompareTag("Ring") && !activated){
+            activated = true;
+            StartCoroutine(Activate());
+            Debug.Log("Colliding with Ring");
+            activated = false;
+        }
+    }
+
+    IEnumerator Activate(){
+        tower.material.color = Color.red;
+
+        yield return new WaitForSeconds(activatedSeconds);
+
+        tower.material.color = basecolour;
+    }
+}
---
+++ Assets/Scripts/ringGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ringGenerator : MonoBehaviour
+{
+    public float bpm = 3;
+    public GameObject ring;
+    public GameObject spawnObj;
+    public float growSpeed = 2;
+    public int targetSizeValue = 5;
+    public float fadeDuration = 1;
+    public float ringHeight = 0.2f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnRingLoop());
+    }
+
+    IEnumerator SpawnRingLoop(){
+        while(true){
+            SpawnRing();
+            yield return new WaitForSeconds(bpm);
+        }
+    }
+
+    void SpawnRing(){
+        GameObject newRing = Instantiate(ring, new Vector3(0,ringHeight,spawnObj.transform.position.z), Quaternion.Euler(90, Random.Range(0, 361), 0));
+        newRing.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        RingBehavior ringScript = newRing.GetComponent<RingBehavior>();
+        ringScript.Init(targetSizeValue, growSpeed, fadeDuration);
+        Debug.Log("Spawned Ring at ");
+    }
+}
