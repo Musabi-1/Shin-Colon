c41489ea4e21bafc2bdddff9cc633c6fb620ae79
koalasushi <kai.daniel.travis@gmail.com>
2025-04-23 17:24:35 +0900

    適当

---
+++ Assets/Scripts/CastleBehavior.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleBehavior : MonoBehaviour
+{
+    public int hp = 100;
+    
+    void Start(){
+
+    }
+
+    void Update(){
+    }
+
+}
